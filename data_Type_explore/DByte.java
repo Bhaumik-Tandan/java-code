cb53b45f919b286f6d4806e753febf82e76eea65
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-08-06 05:11:17 +0530

    ingvored

--- data_Type_explore/DByte.java
+++ data_Type_explore/DByte.java
@@ -1,8 +1,9 @@
 //Range -127 to 128
-public class Dbyte{
+public class DByte{
     public static void main(String args[])
     {
-        byte a=128;
+        //byte a=128;//losely conversion error will come
+        byte a=127;//executed
         System.out.print(a);
     }
 }
