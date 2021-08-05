9f08baf9af8f58ae34712d593ba1854f650defa7
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-08-06 05:20:17 +0530

    giotifnore changed

---
+++ data_Type_explore/Dtype.java
@@ -0,0 +1,11 @@
+//Range -127 to 128
+public class Dtype{
+    public static void main(String args[])
+    {
+        //byte a=128;//losely conversion error will come
+        //byte a=127;//executed
+        //short b=345345;//losely conversion error will come
+        float k=343;//losely conversion error will come
+        float a=343f;
+    }
+}
