1bdfdbebfa6c683929977b684420cef1c73a1bd7
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-08-16 21:45:03 +0530

    io

---
+++ data_Type_explore/io.java
@@ -0,0 +1,9 @@
+public class io
+{
+    public static void main(String args[])
+    {
+        // System.out.printf("%.2f %s %d",98.098,"ojioj",90);//formated output
+        //   System.err.println("err");//error in red sometimes no difference
+        int i=System.in.read(); 
+System.out.println((char)i);
+}}
